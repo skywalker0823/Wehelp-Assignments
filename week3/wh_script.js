21e9ea2189787c6401aad2d99332e17c4a79c54d
skywalker0823 <skywalker0823@hotmail.com>
2022-01-10 21:44:43 +0800

    load pic function complete

--- week3/wh_script.js
+++ week3/wh_script.js
@@ -6,7 +6,6 @@ function burger_list(){
     logo_list.classList.toggle("hide")
     logo_list.classList.toggle("out")
 };
-let counter=1
 function site_in(){
     fetch('https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json')
   .then(function(response) {
@@ -14,6 +13,7 @@ function site_in(){
   })
   .then(function(datas) {
     let data=datas.result.results
+    let counter=1
     for(site of data){
         //console.log(site.stitle,site.file.toLowerCase().split("jpg")[0]+"jpg")
         let site_pic=site.file.toLowerCase().split("jpg")[0]+"jpg";//圖片兒
@@ -40,10 +40,20 @@ function site_in(){
     };
   });
 };
+//記住目前的counter->每點一次more()點亮額外八張圖片1~8已開 9~16 17~24以此類推
+let now_where=9
 function more(){
-    for(let counter=9;counter<17;counter++){
-    document.getElementById("box"+counter).style.display="flex"
-    };
+    if(document.getElementById("box"+now_where).style.display=="none"){
+    for(let counter=now_where;counter<now_where+8;counter++){
+        document.getElementById("box"+counter).style.display="flex";
+    }}
+    // else if(document.getElementById("box18").style.display=="none"){
+    //     for(let counter=now_where;counter<now_where+8;counter++){
+    //         document.getElementById("box"+counter).style.display="flex";
+    //     }
+    // }
+    now_where+=8;
+    console.log(now_where)//這裡是17
 };
 
 // 1. 使用者打開網頁時，立刻透過 JavaScript 連線以上網址，取得景點資料。
